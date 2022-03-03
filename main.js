599ea8a8fff5133294a4fff33690bcdf22ecd5cd
Taleh Didover <taleh.didover@bitspark.de>
2022-03-03 08:47:39 +0100

    Add product name and description

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: {
+                name: "Socks",
+                description: "This is a pair of sock"
+            }
+        }
+    }
+})
